418529b4842ea31c79ee59b9e62de90ba0ee0197
Eliška Kaderová <me@subsonic.cz>
2026-08-11 15:28:42 +0200

    chore: fix ci

--- .dtsconfig.js
+++ .dtsconfig.js
@@ -32,6 +32,7 @@ module.exports = {
       libraries: {
         importedLibraries,
       },
+      noCheck: true,
       output: {
         inlineDeclareGlobals: true,
         inlineDeclareExternals: true
